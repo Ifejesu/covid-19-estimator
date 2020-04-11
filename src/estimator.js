3b5e98099fbafc8e37092f8ffaa01f340eb7d01a
Goodness Ifejesu Olajide <ifejesugoodness97@gmail.com>
2020-04-11 15:59:35 +0100

    feat: challenge 1 to 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,35 @@
-const covid19ImpactEstimator = (data) => data;
+import Estimator from "./myEstimator";
+const covid19ImpactEstimator = (data) => {
+    const {
+        region,
+        periodType,
+        timeToElapse,
+        reportedCases,
+        totalHospitalBeds
+    } = data;
+    const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = region;
+
+    const impact = new Estimator({
+        avgDailyIncomeInUSD,
+        avgDailyIncomePopulation,
+        periodType,
+        timeToElapse,
+        reportedCases,
+        totalHospitalBeds,
+        estimationFactor: 10
+    });
+
+    const severeImpact = new Estimator({
+        avgDailyIncomeInUSD,
+        avgDailyIncomePopulation,
+        periodType,
+        timeToElapse,
+        reportedCases,
+        totalHospitalBeds,
+        estimationFactor: 50
+    });
+
+    return { data, impact: impact.results(), severeImpact: severeImpact.results() };
+};
 
 export default covid19ImpactEstimator;
---
+++ src/myEstimator.js
@@ -0,0 +1,77 @@
+const Estimator = ({
+    avgDailyIncomeInUSD,
+    avgDailyIncomePopulation,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    totalHospitalBeds,
+    estimationFactor
+}) => {
+    this.periodType = periodType;
+    this.timeToElapse = timeToElapse;
+    this.reportedCases = reportedCases;
+    this.totalHospitalBeds = totalHospitalBeds;
+    this.avgDailyIncomePopulation = avgDailyIncomePopulation;
+    this.avgDailyIncomeInUSD = avgDailyIncomeInUSD;
+    this.estimationFactor = estimationFactor;
+};
+
+Estimator.prototype.convertToDays = () => {
+    if (this.periodType === 'days' || this.periodType === 'day') {
+        return this.timeToElapse;
+    }
+    if (this.periodType === 'weeks' || this.periodType === 'week') {
+        return this.timeToElapse * 7;
+    }
+    if (this.periodType === 'months' || this.periodType === 'month') {
+        return this.timeToElapse * 30;
+    }
+};
+
+Estimator.prototype.rateOfInfection = () => {
+    const days = this.convertToDays() / 3;
+
+    return 2 ** Math.trunc(days);
+};
+
+Estimator.prototype.currentlyInfected = () => {
+    return this.reportedCases * this.estimationFactor;
+};
+Estimator.prototype.infectionsByRequestedTime = () => {
+    return this.currentlyInfected() * this.rateOfInfection();
+};
+Estimator.prototype.severeCasesByRequestedTime = () => {
+    return Math.trunc(this.infectionsByRequestedTime() * 0.15);
+};
+Estimator.prototype.hospitalBedsByRequestedTime = () => {
+    return Math.trunc((this.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime());
+};
+
+Estimator.prototype.casesForICUByRequestedTime = () => {
+    return Math.trunc(this.infectionsByRequestedTime() * 0.05);
+};
+
+Estimator.prototype.casesForVentilatorsByRequestedTime = () => {
+    return Math.trunc(this.infectionsByRequestedTime() * 0.02);
+};
+Estimator.prototype.dollarsInFlight = () => {
+    const factor = this.avgDailyIncomePopulation * this.avgDailyIncomeInUSD;
+    const days = this.convertToDays();
+    const res = (this.infectionsByRequestedTime() * factor) / days;
+    return Math.trunc(res);
+};
+
+Estimator.prototype.results = () => {
+    return {
+        currentlyInfected: this.currentlyInfected(),
+        infectionsByRequestedTime: this.infectionsByRequestedTime(),
+        severeCasesByRequestedTime: this.severeCasesByRequestedTime(),
+        hospitalBedsByRequestedTime: this.hospitalBedsByRequestedTime(),
+        casesForICUByRequestedTime: this.casesForICUByRequestedTime(),
+        casesForVentilatorsByRequestedTime: this.casesForVentilatorsByRequestedTime(),
+        dollarsInFlight: this.dollarsInFlight()
+
+    };
+};
+
+export default Estimator;
